bac84d4c8df42205cdfe32aadb4ca428ff41e8c2
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-07-22 19:13:54 +0545

    Initial commit

---
+++ 2_simple_values/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println(1)
+	fmt.Println(1 + 1)
+	fmt.Println(true)
+
+	 x := 1
+	 y := 2
+
+	if x != y {
+		fmt.Println("Correct")
+	} else {
+		fmt.Println("Incorrect")
+	}
+}
