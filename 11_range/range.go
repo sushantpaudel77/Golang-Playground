264a87edff53d95296f43e655abfd5dd9e256891
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-07-24 22:50:12 +0545

    Done with range

--- 11_range/range.go
+++ 11_range/range.go
@@ -6,12 +6,31 @@ import "fmt"
 func main() {
 	nums := []int{5, 6, 7}
 
+	sum := 0
+
 	for _, num := range nums {
-		fmt.Println(num)
+		sum += num
+		fmt.Println(sum)
+	}
+
+	fmt.Println()
+
+	for i, num := range nums {
+		fmt.Println(num, i)
 	}
 
 	// without range
 	// for i := 0; i < len(nums); i++ {
 	// 	fmt.Println(nums[i])
 	// }
+
+	myMap := map[string]string{"fname": "john", "lname": "doe"}
+
+	for k, v := range myMap {
+		fmt.Printf("%s: %s\n", k, v)
+	}
+
+	for _, v := range myMap {
+		fmt.Println(v)
+	}
 }
