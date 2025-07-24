3cf60e77eefab732419ec5582ae302c6933df541
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-07-24 23:41:40 +0545

    ADD: functions

--- 12_functions/functions.go
+++ 12_functions/functions.go
@@ -2,12 +2,35 @@ package main
 
 import "fmt"
 
-func add(a int, b int) int {
-	return a + b
+// func add(a, b int) int {
+// 	return a + b
+// }
+
+func getLanguages() (string, string, string) {
+	return "golang", "java", "python"
+}
+
+// func processIt(fn func(a int) int) {
+// 	fn(1)
+// }
+
+func processIt() func(a int) int {
+	return func(a int) int {
+		return 4
+	}
 }
 
 func main() {
 
-	result := add(7, 7)
-	fmt.Println("Result:", result)
+	// fn := func(a int) int {
+	// 	return 2
+	// }
+
+	fn := processIt()
+	fn(5)
+
+	lang1, lang2, _ := getLanguages()
+	fmt.Println(lang1, lang2)
+	// result := add(7, 7)
+	// fmt.Println("Result:", result)
 }
