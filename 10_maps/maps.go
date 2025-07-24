93218f577cf8603ef24a5c7af72345f9e6752e5c
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-07-24 22:24:18 +0545

    Done with maps

--- 10_maps/maps.go
+++ 10_maps/maps.go
@@ -6,26 +6,47 @@ import "fmt"
 func main() {
 	// creating map
 
-	m := make(map[string]string)
+	// m := make(map[string]string)
 
 	// setting an element
-	m["name"] = "golang"
-	m["area"] = "backend"
+	// m["name"] = "golang"
+	// m["area"] = "backend"
 
 	// get an element
 	// fmt.Println(m["name"], m["area"])
-	fmt.Println(m["phone"])
+	m := make(map[string]int)
+	m["age"] = 30
+	m["price"] = 50
+	// fmt.Println(m["phone"])
+
+	fmt.Println(len(m))
+	clear(m)
+
+	myMap := map[string]int{
+		"price": 70,
+		"phone": 50,
+	}
+
+	_, ok := myMap["phone"]
+
+	if ok {
+		fmt.Println("all ok")
+	} else {
+		fmt.Println("not ok")
+	}
+
+	fmt.Println()
 
 	countryCodes := map[string]string{
 		"AU": "Australia",
 		"US": "United States",
 		"FR": "France",
 		"NP": "Nepal",
+
 	}
 
 	// Add a new element
-	countryCodes["Jp"] = "Japan"
-
+	countryCodes["JP"] = "Japan"
 	// Access an element
 	fmt.Println("NP:", countryCodes["NP"])
 
@@ -37,4 +58,6 @@ func main() {
 
 	// Print the whole map
 	fmt.Println(countryCodes)
+
+
 }
